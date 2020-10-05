4e82abef6a9c6831acc7f862c8fc3140f78fb38f
larry801 <larry.wang.801@gmail.com>
2020-10-05 19:26:21 +0800

    fix lobby server address

--- src/components/lobby.tsx
+++ src/components/lobby.tsx
@@ -4,12 +4,8 @@ import { Lobby } from 'boardgame.io/react';
 import { FilmCentenaryBoard } from './board';
 import {FilmCentenaryGame} from "../Game";
 
-const server = `${window.location.protocol}://${window.location.hostname}:${window.location.port}`;
+const server = `${window.location.protocol}//${window.location.host}`;
 const importedGames = [{ game: FilmCentenaryGame, board: FilmCentenaryBoard }];
-
-export const MUILobby = ()=>{
-
-}
 
 export default () => (
     <div>
