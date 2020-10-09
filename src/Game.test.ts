dcc585fd294f885acd532bd7aede380d3f86d68d
larry801 <larry.wang.801@gmail.com>
2020-10-09 15:34:56 +0800

    3 player test
    Signed-off-by: larry801 <larry.wang.801@gmail.com>

--- src/Game.test.ts
+++ src/Game.test.ts
@@ -12,16 +12,21 @@ const seededGame = gameWithSeed("seed");
 
 it('should declare player 1 as the winner', () => {
     const spec = {
+        numPlayers:3,
         game: seededGame,
         multiplayer: Local(),
     };
 
     const p0 = Client({...spec, playerID: '0'} as any) as any;
     const p1 = Client({...spec, playerID: '1'} as any) as any;
+    const p2 = Client({...spec, playerID: '2'} as any) as any;
 
     p0.start();
     p1.start();
+    p2.start();
+
 
     p0.stop();
     p1.stop();
+    p2.stop();
 })
