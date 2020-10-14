94852e6b0cd5ece319338f8876e6e79bb9843f48
larry801 <larry.wang.801@gmail.com>
2020-10-14 21:40:21 +0800

    Fix 3p vp compensate

--- src/types/setup.ts
+++ src/types/setup.ts
@@ -316,13 +316,17 @@ export const setup = (ctx: Ctx, setupData: any): IG => {
             "B07": 0,
         },
     }
-    if (ctx.numPlayers > 2) {
+    if (ctx.numPlayers === 3) {
         G.regions[Region.NA].buildings[1].activated = true;
         G.regions[Region.WE].buildings[1].activated = true;
-        G.pub[parseInt(G.order[2])].vp = 1;
+        G.pub[parseInt(G.order[1])].vp = 1;
+        G.pub[parseInt(G.order[2])].vp = 2;
     }
-    if (ctx.numPlayers > 3) {
+    if (ctx.numPlayers === 4) {
+        G.regions[Region.NA].buildings[1].activated = true;
+        G.regions[Region.WE].buildings[1].activated = true;
         G.regions[Region.EE].buildings[1].activated = true;
+        G.pub[parseInt(G.order[2])].vp = 1;
         G.pub[parseInt(G.order[3])].vp = 2;
     }
     if (ctx.numPlayers === SimpleRuleNumPlayers) {
