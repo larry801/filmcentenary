1eab9298ed25b6159df8736550e09368ec9061b4
Larry <larry.wang.801@gmail.com>
2021-01-05 10:31:16 +0800

    7,爱森斯坦增加突破效果“将1张手牌放入档案馆”
    Signed-off-by: Larry <larry.wang.801@gmail.com>

--- src/constant/effects.ts
+++ src/constant/effects.ts
@@ -598,7 +598,7 @@ export const effects = {
             ]
         },
         canArchive: (G: IG, ctx: Ctx) => true,
-        archive: noEff,
+        archive: {e: "archive", a: 1},
         response: noResponse,
     },
     "1303": {
