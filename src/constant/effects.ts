2a27402e57631638cbf64e626c147c5dedf44977
Larry <larry.wang.801@gmail.com>
2021-06-15 19:48:34 +0800

    福特顺序调整防止歧义
    Signed-off-by: Larry <larry.wang.801@gmail.com>

--- src/constant/effects.ts
+++ src/constant/effects.ts
@@ -729,9 +729,9 @@ export const effects = {
         canPlay: (G: IG, ctx: Ctx) => true,
         play: {
             e: "step", a: [
+                {e: "draw", a: 1},
                 {e: "noStudio", a: {e: "discardNormalOrLegend", a: 1}},
                 {e: "studio", a: {e: "deposit", a: 1}},
-                {e: "draw", a: 1},
                 {
                     e: "optional", a: {
                         e: "competition", a: {
