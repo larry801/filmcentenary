9105253df6523123d8502c6b2988e070c022fd12
Larry <larry.wang.801@gmail.com>
2021-07-02 19:15:20 +0800

    @全体成员 另外测试一个关于争夺的新机制： 发起方相关规则全部不变 应对方新增如下机制： 应对方亮出手牌  数所有的工业角标，每个角标在结算本次争夺时使你+1竞争力（结算完毕后亮出的手牌正常收回手牌）流派也算
    Signed-off-by: Larry <larry.wang.801@gmail.com>

--- src/game/util.ts
+++ src/game/util.ts
@@ -3380,21 +3380,27 @@ export const startCompetition = (G: IG, ctx: Ctx, atk: PlayerID, def: PlayerID) 
     i.pending = true;
     i.atk = atk;
     i.def = def;
+    let CompetitionPowerModifierDef = 0;
+    const defSchoolID = G.pub[parseInt(i.def)].school;
+    if(defSchoolID !== null){
+        const defSchoolIndustry = getCardById(defSchoolID).industry;
+        CompetitionPowerModifierDef += defSchoolIndustry;
+    }
     const defHands = G.player[parseInt(def)].hand;
     for (const defHand of defHands) {
         const cardIndustryMarkCount = getCardById(defHand).industry;
         if (cardIndustryMarkCount > 0) {
-            G.pub[parseInt(def)].competitionPower += cardIndustryMarkCount;
-            G.competitionInfo.defShownCards.push(defHand);
+            CompetitionPowerModifierDef += cardIndustryMarkCount;
         }
+        G.competitionInfo.defShownCards.push(defHand);
     }
-    const CompetitionPowerDelta = G.pub[parseInt(atk)].competitionPower - G.pub[parseInt(def)].competitionPower;
-    log.push(`|CompetitionPowerDelta:${CompetitionPowerDelta}`)
+    const CompetitionPowerDelta = G.pub[parseInt(atk)].competitionPower - G.pub[parseInt(def)].competitionPower - CompetitionPowerModifierDef;
+    log.push(`|CompetitionPowerDelta:${CompetitionPowerDelta}`);
     i.progress = CompetitionPowerDelta;
     // if (CompetitionPowerDelta >= 3) {
     //     hasWinner = true;
     // }
-    const defSchoolID = G.pub[parseInt(i.def)].school;
+
     if (defSchoolID !== SchoolCardID.S3201 && defSchoolID !== SchoolCardID.S3204) {
         log.push(`|p${i.def}|lose${CompetitionPowerDelta}vp`);
         loseVp(G, ctx, def, CompetitionPowerDelta);
