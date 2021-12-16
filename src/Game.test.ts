314f9ed08d3711f0201b0ba36d1020bda1ee7745
Larry Wang <larry.wang.801@gmail.com>
2021-12-16 18:52:11 +0800

    remove test fix github actions
    Signed-off-by: Larry Wang <larry.wang.801@gmail.com>

--- src/Game.test.ts
+++
@@ -1,6 +0,0 @@
-import {doNotLoseVpAfterCompetitionSchool} from "./game/util";
-import {SchoolCardID} from "./types/core";
-
-test('3302', ()=>{
-    console.log(doNotLoseVpAfterCompetitionSchool(SchoolCardID.S3201));
-});
